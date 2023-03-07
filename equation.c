1d8008eea78b947b2e3d18b2ace0142740b859a1
Mila <gorbunova.milasha@gmail.com>
2023-03-07 06:35:34 +0200

    decomposition of validation func + started file mode
    now there is validation func, dynamic string implementation func, and one for the inraracting mode

--- equation.c
+++ equation.c
@@ -5,19 +5,14 @@
 #include<regex.h>
 #include<math.h>
 
-long double validation(char name){
-  int valid = 0;
-	long double result = 0;
-
-	do {
-  printf("%c = ", name);
-
-//dynamic string
-  char *str, c=0;
+char * dyn_str_implement (char name) {
+//dynamic string implementation
+	printf("%c = ", name);
+  char *str, c = 0;
   int i = 0, j = 1;
   str = (char*)malloc(sizeof(char));
   while (c != '\n') {
-    c = getc(stdin);
+    c = fgetc(stdin);
     str = (char*)realloc(str, j * sizeof(char));
     str[i] = c;
     i++;
@@ -25,6 +20,11 @@ long double validation(char name){
   }
 
   str[i-1] = '\0'; // \0 isted of \n to mark end of string
+	return str;
+}
+
+int validation(char name, char * str){
+	long double result = 0;
 
 	//validate input with regex
 	regex_t regex;
@@ -33,24 +33,29 @@ long double validation(char name){
 					if (reti) {
 							printf("Error. Expected a valid real number, got %s instead\n", str);
 							regfree(&regex);
-							free(str);
-							continue;
+							return 0;
 					}
 	regfree(&regex);
 
 	sscanf(str, "%Lf", &result);
-
 	if (name == 'a' && result == 0) {
 		printf("Error. This is a quadratic equation -> `a` cannot be equal to 0\n");
-		free(str);
-		continue;
+		return 0;
 	}
 
-  free(str);
-	valid = 1;
-	} while(valid == 0);
+  return 1;
+}
 
-  return result;
+long double intaract_mode_implement(char name) {
+  	int isValid = 0;
+		long double result = 0;
+		while (isValid == 0) {
+			char *str = dyn_str_implement(name);
+			isValid = validation(name, str);
+			if (isValid) sscanf(str, "%Lf", &result);
+			free(str);
+		}
+	return result;
 }
 
 void solve_quadratic_equation(long double a, long double b, long double c) {
@@ -71,10 +76,41 @@ void solve_quadratic_equation(long double a, long double b, long double c) {
 	}
 }
 
-void main() {
-  long double a = validation('a');
-  long double b = validation('b');
-  long double c = validation('c');
+int main(int argc, char **argv) {
+	long double a = 0, b = 0, c = 0;
 
-	solve_quadratic_equation(a, b, c);
+	if (argc == 1) {//інтерактивний режим
+		long double a = intaract_mode_implement('a');
+		long double b = intaract_mode_implement('b');
+		long double c = intaract_mode_implement('c');
+	}
+
+	else {//файловий
+		FILE *fp;
+		char *filename;
+		char ch;
+
+		filename = argv[1];
+    printf("Filename : %s\n", filename);
+
+   // Open file in read-only mode
+   fp = fopen(filename,"r");
+
+   // If file opened successfully, then print the contents
+   if ( fp )
+      {
+        printf("File contents:\n");
+        while ( (ch = fgetc(fp)) != EOF )
+           {
+                printf("%c",ch);
+           }
+				printf("\n");
+       }
+   else
+      {
+         printf("file %s does not exist\n", filename);
+        }
+	}
+	//solve_quadratic_equation(a, b, c);
+	return 0;
 }
